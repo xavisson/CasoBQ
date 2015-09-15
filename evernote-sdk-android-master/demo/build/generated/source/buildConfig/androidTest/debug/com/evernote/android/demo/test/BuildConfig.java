88c75070030bc7f8178e0a43672419101c541f2f
xavisson <javier.delpalacio@nexstreaming.com>
2015-09-16 00:49:28 +0200

    merged from develop. First release

---
+++ evernote-sdk-android-master/demo/build/generated/source/buildConfig/androidTest/debug/com/evernote/android/demo/test/BuildConfig.java
@@ -0,0 +1,18 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.evernote.android.demo.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.evernote.android.demo.test";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "2.0.0-RC3";
+  // Fields from default config.
+  public static final String EVERNOTE_CONSUMER_KEY = "Your consumer key";
+  public static final String EVERNOTE_CONSUMER_SECRET = "Your consumer secret";
+  public static final String EVERNOTE_DEVELOPER_KEY = null;
+  public static final String EVERNOTE_NOTE_STORE_URL = null;
+}
